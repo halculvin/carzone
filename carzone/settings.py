2b9914a8fdf25463dac9e3c4137d2e5192d35512
halculvin <50222922+halculvin@users.noreply.github.com>
2022-03-16 18:38:50 -0700

    Update settings.py

--- carzone/settings.py
+++ carzone/settings.py
@@ -158,5 +158,5 @@ SITE_ID = 1
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'halculvin@gmail.com'
-EMAIL_HOST_PASSWORD = 'hmyeojuxyliuccee'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
